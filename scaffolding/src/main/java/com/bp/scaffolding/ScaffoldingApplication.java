58ec422b60d5fe070413199bd51d1b3f66151efb
Hansiyuan131 <422295068@qq.com>
2022-02-28 18:07:09 +0800

    添加Spring Boot脚手架code

---
+++ scaffolding/src/main/java/com/bp/scaffolding/ScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package com.bp.scaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldingApplication.class, args);
+    }
+
+}
---
+++ scaffolding/src/test/java/com/bp/scaffolding/ScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bp.scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3134df3e3d16df33e13701552ac7b6dcd23dfd00
Hansiyuan131 <422295068@qq.com>
2022-03-02 13:14:45 +0800

    添加反射代码

---
+++ core/src/main/java/com/bp/core/advance/jvm/reflect/AdTv.java
@@ -0,0 +1,14 @@
+package com.bp.core.advance.jvm.reflect;
+
+/**
+ * @description: 广告电视屏
+ * @author: hansiyuan
+ * @date: 2022/3/2 11:43 AM
+ */
+public class AdTv implements TV {
+
+    @Override
+    public void show() {
+        System.out.println("播放广告....");
+    }
+}
---
+++ core/src/main/java/com/bp/core/advance/jvm/reflect/TV.java
@@ -0,0 +1,13 @@
+package com.bp.core.advance.jvm.reflect;
+
+/**
+ * @description: TV电视
+ * @author: hansiyuan
+ * @date: 2022/3/2 11:40 AM
+ */
+public interface TV {
+    /**
+     * 电视屏展示内容
+     */
+    void show();
+}

abf219655df75495c6cbf41b7043914459f82e92
Hansiyuan131 <422295068@qq.com>
2022-03-03 21:22:25 +0800

    添加GC日志分析程序

--- core/src/main/java/com/bp/core/advance/jvm/classloader/HelloClassLoader.java
+++ core/src/main/java/com/bp/core/advance/jvm/classloader/HelloClassLoader.java
@@ -2,15 +2,14 @@ package com.bp.core.advance.jvm.classloader;
 
 import java.util.Base64;
 
+
+/**
+ * @description: 自定义类加载器
+ * @author: hansiyuan
+ * @date: 2022/3/3 11:00 AM
+ */
 public class HelloClassLoader extends ClassLoader {
     public static void main(String[] args) {
-        String source = "Compiled from \"Hello.java\"\n" +
-                "public class com.bp.core.advance.jvm.classloader.Hello {\n" +
-                "  public com.bp.core.advance.jvm.classloader.Hello();\n" +
-                "  static {};\n" +
-                "}";
-        String encode = Base64.getEncoder().encodeToString(source.getBytes());
-        System.out.println(encode);
         try {
             new HelloClassLoader().findClass("com.bp.core.advance.jvm.classloader.Hello").newInstance();
         } catch (Exception e) {
@@ -20,14 +19,10 @@ public class HelloClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) {
-//        String helloBase64 = "Q29tcGlsZWQgZnJvbSAiSGVsbG8uamF2YSIKcHVibGljIGNsYXNzIGNvbS5icC5jb3JlLmFkdmFuY2UuanZtLmNsYXNzbG9hZGVyLkhlbGxvIHsKICBwdWJsaWMgY29tLmJwLmNvcmUuYWR2YW5jZS5qdm0uY2xhc3Nsb2FkZXIuSGVsbG8oKTsKICBzdGF0aWMge307Cn0=";
-//        byte[] bytes = decode(helloBase64);
-        String source = "public class com.bp.core.advance.jvm.classloader.Hello {\n" +
-                "  public com.bp.core.advance.jvm.classloader.Hello();\n" +
-                "  static {};\n" +
-                "}";
-        byte[] bytes = source.getBytes();
-
+        // 字节码base64编码后字符串
+        String helloBase64 = "Q29tcGlsZWQgZnJvbSAiSGVsbG8uamF2YSIKcHVibGljIGNsYXNzIGNvbS5icC5jb3JlLmFkdmFuY2UuanZtLmNsYXNzbG9hZGVyLkhlbGxvIHsKICBwdWJsaWMgY29tLmJwLmNvcmUuYWR2YW5jZS5qdm0uY2xhc3Nsb2FkZXIuSGVsbG8oKTsKICBzdGF0aWMge307Cn0=";
+        // 编码为字节数组
+        byte[] bytes = decode(helloBase64);
         // 将字节数组转换为类的实例。
         return defineClass(name, bytes, 0, bytes.length);
     }

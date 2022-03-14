1eee4e2f9720ae8a726bef67fda5b086d9ab49d1
Hansiyuan131 <422295068@qq.com>
2022-03-14 21:02:11 +0800

    添加NIO示例代码

---
+++ core/src/main/java/com/bp/core/advance/netty/NettyChatServer.java
@@ -0,0 +1,124 @@
+package com.bp.core.advance.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author hansiyuan
+ * @date 2022年03月11日 23:00
+ */
+public final class NettyChatServer {
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new LoggingHandler(LogLevel.INFO));
+                            p.addLast(new ChatNettyHandler());
+                        }
+                    });
+            ChannelFuture f = serverBootstrap.bind(PORT).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private static class ChatNettyHandler extends SimpleChannelInboundHandler<ByteBuf> {
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("one conn active: " + ctx.channel());
+            ChatHolder.join((SocketChannel) ctx.channel());
+        }
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, ByteBuf byteBuf)
+                throws Exception {
+            byte[] bytes = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(bytes);
+            String content = new String(bytes, StandardCharsets.UTF_8);
+            System.out.println(content);
+            if ("quit\r\n".equals(content)) {
+                ctx.channel().close();
+            } else {
+                ChatHolder.propagate((SocketChannel) ctx.channel(), content);
+            }
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println("one conn inactive: " + ctx.channel());
+            ChatHolder.quit((SocketChannel) ctx.channel());
+        }
+    }
+
+    private static class ChatHolder {
+        static final Map<SocketChannel, String> USER_MAP = new
+                ConcurrentHashMap<>();
+
+        static void join(SocketChannel socketChannel) {
+            String userId = "用户" + ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+            send(socketChannel, "您的id为：" + userId + "\n\r");
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                send(channel, userId + " 加入了群聊" + "\n\r");
+            }
+            USER_MAP.put(socketChannel, userId);
+        }
+
+        static void quit(SocketChannel socketChannel) {
+            String userId = USER_MAP.get(socketChannel);
+            send(socketChannel, "您退出了群聊" + "\n\r");
+            USER_MAP.remove(socketChannel);
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                if (channel != socketChannel) {
+                    send(channel, userId + " 退出了群聊" + "\n\r");
+                }
+            }
+        }
+
+        public static void propagate(SocketChannel socketChannel, String
+                content) {
+            String userId = USER_MAP.get(socketChannel);
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                if (channel != socketChannel) {
+                    send(channel, userId + ": " + content);
+                }
+            }
+        }
+
+        static void send(SocketChannel socketChannel, String msg) {
+            try {
+                ByteBufAllocator allocator = ByteBufAllocator.DEFAULT;
+                ByteBuf writeBuffer = allocator.buffer(msg.getBytes().length);
+                writeBuffer.writeCharSequence(msg, Charset.defaultCharset());
+                socketChannel.writeAndFlush(writeBuffer);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ core/src/main/java/com/bp/core/advance/nio/BioEchoServer.java
@@ -0,0 +1,33 @@
+package com.bp.core.advance.nio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author hansiyuan
+ * @date 2022年03月11日 22:36
+ */
+public class BioEchoServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8001);
+        System.out.println("端口8001服务启动");
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("连接成功: " + socket);
+            new Thread(() -> {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String msg;
+                    while ((msg = reader.readLine()) != null) {
+                        System.out.println("接收消息: " + msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}

000a9d06c8c8fea959621b8d8ff1f3696f6ae06f
Hansiyuan131 <422295068@qq.com>
2022-03-15 18:10:02 +0800

    策略代码

---
+++ core/src/main/java/com/bp/core/design/strategy/StrategyApplication.java
@@ -0,0 +1,9 @@
+package com.bp.core.design.strategy;
+
+/**
+ * @description: 策略的使用
+ * @author: hansiyuan
+ * @date: 2022/3/15 10:12 AM
+ */
+public class StrategyApplication {
+}
---
+++ core/src/main/java/com/bp/core/design/strategy/StrategyFactoryV2.java
@@ -0,0 +1,20 @@
+package com.bp.core.design.strategy;
+
+/**
+ * @description: 策略使用V2
+ * @author: hansiyuan
+ * @date: 2022/3/15 10:11 AM
+ */
+public class StrategyFactoryV2 {
+    public static Strategy getStrategy(String type) {
+        if (type == null || type.isEmpty()) {
+            throw new IllegalArgumentException("type should not be empty.");
+        }
+        if ("A".equals(type)) {
+            return new ConcreteStrategyA();
+        } else if ("B".equals(type)) {
+            return new ConcreteStrategyB();
+        }
+        return null;
+    }
+}

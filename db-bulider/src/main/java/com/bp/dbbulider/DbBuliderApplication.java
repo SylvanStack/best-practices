cd9b9e2a2baedd47dd375e62daf8614bdea42a18
Hansiyuan131 <422295068@qq.com>
2022-03-27 22:32:43 +0800

    db-bulider

---
+++ db-bulider/src/main/java/com/bp/dbbulider/DbBuliderApplication.java
@@ -0,0 +1,13 @@
+package com.bp.dbbulider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBuliderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbBuliderApplication.class, args);
+    }
+
+}
---
+++ db-bulider/src/test/java/com/bp/dbbulider/DbBuliderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bp.dbbulider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBuliderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

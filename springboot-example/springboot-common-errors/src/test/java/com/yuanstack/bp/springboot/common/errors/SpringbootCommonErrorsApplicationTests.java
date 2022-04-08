ccde9a812bb30bb949e25e6664b1095cc2b49f86
Hansiyuan131 <422295068@qq.com>
2022-04-08 11:14:57 +0800

    yuan,feat: spring常见错误

---
+++ springboot-example/springboot-common-errors/src/test/java/com/yuanstack/bp/springboot/common/errors/SpringbootCommonErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanstack.bp.springboot.common.errors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonErrorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
